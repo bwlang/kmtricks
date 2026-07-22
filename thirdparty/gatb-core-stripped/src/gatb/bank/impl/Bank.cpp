a281ccd2cb02b2869646f6f9ed80089e6ddd11ec
Brad Langhorst <langhorst@neb.com>
2026-07-22 17:20:44 -0400

    Add BAM format support with BankBam implementation and related classes

--- thirdparty/gatb-core-stripped/src/gatb/bank/impl/Bank.cpp
+++ thirdparty/gatb-core-stripped/src/gatb/bank/impl/Bank.cpp
@@ -20,6 +20,7 @@
 #include <gatb/bank/impl/Bank.hpp>
 
 #include <gatb/bank/impl/BankFasta.hpp>
+#include <gatb/bank/impl/BankBam.hpp>
 
 #include <gatb/bank/impl/BankBinary.hpp>
 #include <gatb/bank/impl/BankAlbum.hpp>
@@ -47,6 +48,7 @@ Bank::Bank ()
     /** We register most known factories. */
     _registerFactory_ ("album",  new BankAlbumFactory(),  false);
     _registerFactory_ ("fasta",  new BankFastaFactory(),  false);
+    _registerFactory_ ("bam",    new BankBamFactory(),    false);
     _registerFactory_ ("binary", new BankBinaryFactory(), false);
 
     DEBUG (("Bank::Bank,  found %ld factories\n", _factories.size()));
